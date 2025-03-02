f59184acfe9600f0d7ba744284f47950a9b106fc
Caeszx. <ravensvenix@gmail.com>
2025-03-03 01:47:03 +0800

    Delete rwMem Driver

--- drivers/rwMem/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo " " | tee -a ../Makefile
-echo -n 'obj-$(CONFIG_RWMEM)		+= rwmem/' | tee -a ../Makefile
-
-sed -i "/endmenu/i\source \"drivers/rwmem/Kconfig\"" ../Kconfig
-sed -i "/endmenu/i\ " ../Kconfig
