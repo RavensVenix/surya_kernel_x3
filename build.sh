9a863429f4e2e8bd7a1ea802d5a41abdca68513e
Caeszx. <ravensvenix@gmail.com>
2025-03-02 17:30:11 +0800

    Fuck it, I destroyed everything.

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@
 
 SECONDS=0 # builtin bash timer
 ZIPNAME="Elaina-surya-$(date '+%Y%m%d-%H%M').zip"
+ZIPNAME_KSU="Elaina-KernelSU-Next-surya-$(date '+%Y%m%d-%H%M').zip"
 TC_DIR="$(pwd)/tc/clang-20"
 AK3_DIR="$(pwd)/android/AnyKernel3"
 DEFCONFIG="surya_defconfig"
@@ -88,7 +89,7 @@ for arg in "$@"; do
 			;;
 		-s|--su)
 			ENABLE_KSU=true
-			ZIPNAME="${ZIPNAME/Elaina-surya/Elaina-KSU}"
+			ZIPNAME="${ZIPNAME_KSU/Elaina-surya/Elaina-KSU}"
 			;;
 		*)
 			echo "Unknown argument: $arg"
